20b80492698a7d216cd2619a80a940231eb0b5c5
spapa013 <stelios@spapa.us>
2023-10-19 14:01:35 -0500

    adds proximity adapters and externals

--- python/microns-proximities-api/microns_proximities_api/config/adapters.py
+++ python/microns-proximities-api/microns_proximities_api/config/adapters.py
@@ -1,2 +1,6 @@
-minnie_proximities = {}
+from microns_utils.adapter_utils import NumpyAdapter
 
+minnie_proximities = {
+    'minnie65_proximity_skeletons': NumpyAdapter('filepath@minnie65_proximity_skeletons'),
+    'minnie65_proximity_results': NumpyAdapter('filepath@minnie65_proximity_results'),
+}
