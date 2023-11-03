97b79d27524d8fb13c281869966161c25795b3ce
spapa013 <stelios@spapa.us>
2023-11-03 16:30:37 -0500

    update proximity deploy

--- deploy/kubernetes/minnie65_compute_proximities.py
+++ deploy/kubernetes/minnie65_compute_proximities.py
@@ -1,4 +1,4 @@
 if __name__ == '__main__':
     from microns_proximities.minnie_proximities import minnie_proximities as mp
-    mp.ProximityMaker.SkeletonProcessedSetChunk.populate({'skeleton_prc_set_chunk': '8fe31e81'}, reserve_jobs=True, order='random')
+    mp.ProximityMaker.SkeletonProcessedSetChunk.populate(reserve_jobs=True, order='random', suppress_errors=True)
 
