251595116e881779ca3e38daa8eabddb47b749ab
spapa013 <stelios@spapa.us>
2023-11-03 16:31:51 -0500

    update external from minnie65_proximity_results to minnie65_proximities

--- python/microns-proximities-api/microns_proximities_api/config/adapters.py
+++ python/microns-proximities-api/microns_proximities_api/config/adapters.py
@@ -2,5 +2,5 @@ from microns_utils.adapter_utils import NumpyAdapter
 
 minnie_proximities = {
     'minnie65_proximity_skeletons': NumpyAdapter('filepath@minnie65_proximity_skeletons'),
-    'minnie65_proximity_results': NumpyAdapter('filepath@minnie65_proximity_results'),
+    'minnie65_proximities': NumpyAdapter('filepath@minnie65_proximities'),
 }
